63b3a8d1f7c2accd5095757bb2190ca49d1a4291
sanchitapatil123 <patilsanchita025@gmail.com>
2025-06-11 09:45:50 +0530

    Add Checknumber1.java

---
+++ Checknumber1.java
@@ -0,0 +1,19 @@
+//implementing the java project that detaermines if a number entered by the user is positive, negative,zero
+//creating the class for to check weather the number is positive or negative or zero 
+//if the number is postive push to main 
+//if negative and zero push to master
+public class Checknumber1{
+    public static void main(String[] args){
+        int number = -20;
+        System.out.println("lets check the Number !");
+        if(number >0){
+            System.out.println(" Heyy! The Given Number is NEGATIVE INTEGER !");
+        }else if(number==0){
+            System.out.println("HEYYY ! THE GIVEN NUMBER IS ZERO......!")
+        }
+        else{
+                System.out.println("Oops ! The Given Number is not NEGATIVE INTEGER NUMBER nor The Zero ! Try again ....");
+            }
+        }
+    }
+}
